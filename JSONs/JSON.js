0f4268982d425c3fbaa14bbef7477f38f5dd28c5
IStolbtsov <stigor1991@mail.com>
2023-08-31 19:50:36 +0300

    2444

---
+++ JSONs/JSON.js
@@ -0,0 +1,25 @@
+// let obj1 = {name: "Igor", lname: "Stolbtsov", age: "100500"}
+
+// console.log(obj1)
+
+// let jsonObj1 = JSON.stringify(obj1)
+
+// console.log(jsonObj1)
+
+const values = require('./values.json')
+console.log(values)
+
+// let jsonObj1 = '{"name":"Igor", "lname": "Stolbtsov", "age": "100500"}'
+
+// let obj2 = JSON.parse(jsonObj1)
+
+// console.log(obj2.name)
+
+// obj2.city = "York"
+
+// console.log(obj2)
+
+// jsonObj1 = JSON.stringify(obj2)
+
+//console.log(jsonObj1)
+
