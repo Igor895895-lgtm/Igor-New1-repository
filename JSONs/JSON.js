944844148845765b5909040bd8926401c85daf59
IStolbtsov <stigor1991@mail.com>
2023-09-03 23:40:10 +0300

    1234

--- JSONs/JSON.js
+++ JSONs/JSON.js
@@ -6,8 +6,71 @@
 
 // console.log(jsonObj1)
 
-const values = require('./values.json')
-console.log(values)
+//const values = require('./values.json')
+
+// const fs = require('fs-extra')
+const { writeFile, readFile } = require('fs');
+
+const newData = {phone: 434224, adress: 'Street'}
+
+
+
+// readFile(path, (error, data) => {
+//     if (error) {
+//       console.log(error);
+//       return;
+//     }
+//     const parsedData = JSON.parse(data);
+//     parsedData.createdAt = new Date().toISOString();
+//     writeFile(path, JSON.stringify(parsedData, null, 2), (err) => {
+//       if (err) {
+//         console.log('Failed to write updated data to file');
+//         return;
+//       }
+//       console.log('Updated file successfully');
+//     });
+//   });
+
+
+
+  //try{code with problems} catch (err) {cons}
+// try{
+//     fs.readFile(path, 'utf8', (error, data) => {
+//     if(error) {
+//         console.log('Error during reading file')
+//     } else {
+//         readData = JSON.parse(data)
+//     }
+//     readData.address = 'Street1'
+
+// }
+// })
+
+//fs.writeFile(path, JSON.stringify(readData));
+
+// } catch (err) {
+//     console.log(`Error during code actions: ${err}`)
+
+// function readD() {
+//     fs.writeFile(path).then(rData) => {
+//         return
+//     }).then(nData) => {console.log(nData)}
+
+// }
+// function Func() {
+//     fetch('./values.json')
+//         .then((res) => {
+//         return res.json();
+//     })
+//     .then((data) => console.log(data));
+// }
+// Func()
+
+// console.log(dataFromFile)
+
+//fs.writeFile(path, JSON.stringify(newData));
+
+// console.log(values)
 
 // let jsonObj1 = '{"name":"Igor", "lname": "Stolbtsov", "age": "100500"}'
 
@@ -23,3 +86,32 @@ console.log(values)
 
 //console.log(jsonObj1)
 
+
+
+
+
+
+const fs = require('fs-extra');
+let path = "./values.json";
+let data, jsonData;
+    try {
+    if(fs.existsSync(path)) {
+    data = fs.readFileSync(path, 'utf8');
+    if(data) {
+     jsonData = JSON.parse(data);
+    if(typeof jsonData === 'object' && jsonData !== null && !Array.isArray(jsonData)) {
+    jsonData.test = 'test';
+    jsonData.email = 'test@gmail.com';
+    fs.writeFileSync(path, JSON.stringify(jsonData));
+    } else {
+                 throw new Error("Parsed JSON data is not an object");
+       }
+  } else {
+    throw new Error("No data read from the file");
+       }
+ } else {
+      throw new Error("File does not exist");
+   }
+ } catch (err) {
+   console.error(err.message);
+ }
---
+++ JSONs/function.js
@@ -0,0 +1,42 @@
+function clickButton() {
+    console.log('Click the element')
+    frameworkPlaywright.clickTheButton()
+}
+let = ownVariable1 = 'sss'
+
+// function clickButton(selector, text='Hello') {
+//     console.log('Click the element')
+//    // frameworkPlaywright.clickTheButton(selector)
+//     ownVariable1 = 'local value'
+//     //assert txtOnUI == text
+// }
+
+// clickButton
+// console.log(ownVariable1)
+
+// function clickButton2(selector, text='Hello') {
+// console.log('Click the element')
+// let ownVariable2 = 'local value'
+// clickButton()
+// return ownVariable2
+// let ownVar22 = 'another local value'
+// return ownVar22
+
+
+// }
+
+// let valueFromFunction = clickButton()
+// console.log(valueFromFunction)
+
+//FUNCTION EXPRESSION
+
+let getTheValue = function() {
+    console.log('Fetching the data from UI')
+    //frameworkPlaywright.getTheText()
+}
+
+let copygetTheValue = getTheValue
+
+copygetTheValue()
+
+//console.log(getTheValue())
